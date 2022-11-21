5ba5ab68c2cf271e71355c12b76c7f84724b5f4f
Jonathan Ehman <jonathan.ehman@mdaemon.com>
2022-11-21 16:53:32 -0600

    Updated rollup and dist files

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default [
@@ -8,17 +9,30 @@ export default [
 		output: {
 			name: 'validate',
 			file: pkg.browser,
-			format: 'umd'
+			format: 'umd',
+			exports: 'named'
 		},
 		plugins: [
-			commonjs() // so Rollup can convert `ms` to an ES module
+			commonjs(), // so Rollup can convert `ms` to an ES module
+			terser()
 		]
 	},
 	{
 		input: 'src/validate.js',
 		output: [
-			{ file: pkg.main, format: 'cjs', exports: 'default' },
-			{ file: pkg.module, format: 'es' }
+			{ 
+				file: pkg.main,
+				format: 'cjs',
+				exports: 'named'
+			},
+			{ 
+				file: pkg.module, 
+				format: 'es',
+				exports: 'named'
+			}
+		],
+		plugins: [ 
+			terser()
 		]
 	}
 ]
